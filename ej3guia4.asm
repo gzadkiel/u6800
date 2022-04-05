cfe8634cc9b82ba40a4ae06a664686af23b30793
transconductancia <62406795+gzadkiel@users.noreply.github.com>
2022-04-05 10:35:01 -0300

    4 bytes version (unnecessary)

--- ej3guia4.asm
+++ ej3guia4.asm
@@ -2,9 +2,8 @@ ROMStart	equ $F600
 RAMStart	equ $0080
 Vectores	equ $FFF8
 
-;;se pierde A, se sobreescribe con el resultado PROMEDIO
+;;A is lost in the process, it gets overwritten by the result of the operation
 
-;;ingreso como nums de 4 bytes 
 AH equ $0007
 AL equ $E2B5
 BH equ $003F
@@ -27,48 +26,48 @@ BLL ds 1
 
 Inicio
 
-	ldhx #AH					;cargo la parte alta de A, osea 00 y el terce bit	
-	sthx RESHH				;guardo  		
-	ldhx #AL					;cargo la parte baja de A, los dos bytes lsb
-	sthx RESLH 				;guardo
-    ldhx #BH
-    sthx BHH
-    ldhx #BL
-    sthx BLH
+	ldhx #AH				
+	sthx RESHH						
+	ldhx #AL
+	sthx RESLH 
+	ldhx #BH
+ 	sthx BHH
+    	ldhx #BL
+    	sthx BLH
 	jsr SUMA
-    asr RESHL
-    ror RESLH
-    ror RESLL
-    bra fin
+    	asr RESHL
+    	ror RESLH
+    	ror RESLL
+    	bra fin
 
 
 SUMA
 	
-    clc 
+    	clc 
 	lda RESLL
 	add BLL
 	sta RESLL
 	lda RESLH
 	adc BLH
 	sta RESLH
-    lda RESHL
-    adc BHL
-    sta RESHL
-    lda RESHH
-    adc BHH
-    sta RESHH
+    	lda RESHL
+  	adc BHL
+    	sta RESHL
+    	lda RESHH
+    	adc BHH
+    	sta RESHH
 	rts
 
 
 fin 
-		bra fin
+	bra fin
 
 Vacio
 
-	 	rti
-		org  Vectores  
+	rti
+	org  Vectores  
 
-    dw   Vacio     
-    dw   Vacio
-    dw   Vacio            
-    dw   Inicio          
+    	dw   Vacio     
+    	dw   Vacio
+    	dw   Vacio            
+    	dw   Inicio          
